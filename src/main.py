24f4c6aab2a261e91f962543ff17f912a572ab88
Camari Middlebrooks <camari.@Mac.attlocal.net>
2026-06-29 21:34:47 -0400

    experimenting with google search grounding to gemini requests

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 import os
 
@@ -6,3 +7,28 @@ load_dotenv()
 
 api_key = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
+
+config = types.GenerateContentConfig(
+    tools = [types.Tool(google_search=types.GoogleSearch())]
+)
+
+
+
+response = client.models.generate_content(
+    model = "gemini-2.5-flash",
+    contents = "Who has scored the most goals in the 2026 World Cup as of right now?",
+    config = config
+
+)
+print(response.text)
+
+metadata = response.candidates[0].grounding_metadata
+if metadata.web_search_queries:
+    print("\nSearch queries executed: ")
+    for query in metadata.web_search_queries:
+        print(f" - {query}")
+
+if metadata.grounding_chunks:
+    print("\nSources: ")
+    for chunk in metadata.grounding_chunks:
+        print(f" - [{chunk.web.title}]({chunk.web.uri})")

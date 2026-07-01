5a209a4a7712daeee4288f574e46a33fff4f157d
Camari Middlebrooks <camari.@Mac.attlocal.net>
2026-06-30 22:39:41 -0400

    implemented memory loop

--- src/main.py
+++ src/main.py
@@ -8,12 +8,13 @@ load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+purpose_model = "gemini-3.5-flash"
 config = types.GenerateContentConfig(
     tools = [types.Tool(google_search=types.GoogleSearch())]
 )
 
 
-
+"""
 response = client.models.generate_content(
     model = "gemini-2.5-flash",
     contents = "Who has scored the most goals in the 2026 World Cup as of right now?",
@@ -32,3 +33,30 @@ if metadata.grounding_chunks:
     print("\nSources: ")
     for chunk in metadata.grounding_chunks:
         print(f" - [{chunk.web.title}]({chunk.web.uri})")
+
+"""
+
+previous_interaction_id = None
+while True:
+    prompt = input("What would you like to know? ")
+    if prompt.lower() == "nothing":
+        break
+    
+
+    interaction1 = client.interactions.create(
+        model="gemini-3.5-flash",
+        input=prompt,
+        previous_interaction_id=previous_interaction_id
+        
+    )
+    print(interaction1.output_text)
+    previous_interaction_id = interaction1.id
+
+"""
+    interaction2 = client.interactions.create(
+        model = "gemini-3.5-flash",
+        input = "How did this person become who they are? (short versrion)",
+        previous_interaction_id=interaction1.id,
+
+    )
+print(interaction2.output_text) """

4c23710c0bce0b927657346d05b102bfcac42283
Camari Middlebrooks <camari.@Camaris-MacBook-Air.local>
2026-08-25 19:50:38 -0400

    Add production frontend URL to  CORS allowed origins

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,8 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://ai-content-brief-tool-ids4ee5m2-prexcre.vercel.app"
 ]
 
 app.add_middleware(
